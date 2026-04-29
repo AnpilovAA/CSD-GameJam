624b1ab16d099a01a8f333ff35a7f81ed78daf36
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2026-04-29 12:19:07 +0300

    Дописал прыжок и движение по вектору x движения персонажа

--- new-game-project/main/player.gd
+++ new-game-project/main/player.gd
@@ -1,11 +1,28 @@
 extends Area2D
 
+@export var speed = 300
+
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += 1
+		if Input.is_action_just_pressed("move_right"):
+			velocity.x += 1
+		if Input.is_action_just_pressed("move_left"):
+			velocity.x -= 1
+	if Input.is_action_just_released("roll"):
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 10
+		elif Input.is_action_pressed("move_left"):
+			velocity.x -= 10
