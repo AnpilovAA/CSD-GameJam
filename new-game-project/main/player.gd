34b50d051da910c5be57e7a2bca4a31bb2375585
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2026-04-29 12:52:34 +0300

    Добавил таймер для уворота.

--- new-game-project/main/player.gd
+++ new-game-project/main/player.gd
@@ -21,8 +21,17 @@ func _process(delta: float) -> void:
 			velocity.x += 1
 		if Input.is_action_just_pressed("move_left"):
 			velocity.x -= 1
+
 	if Input.is_action_just_released("roll"):
-		if Input.is_action_pressed("move_right"):
+		#Тут я добавил таймаут для roll что бы не был возможности спамить
+		if Input.is_action_pressed("move_right") and $Timer.is_stopped():
 			velocity.x += 10
-		elif Input.is_action_pressed("move_left"):
+			$Timer.start(2.0)
+		elif Input.is_action_pressed("move_left") and $Timer.is_stopped():
 			velocity.x -= 10
+			$Timer.start(2.0)
+   
+   
+func _on_timer_timeout() -> void:
+	$Timer.stop()
+	print("Timer off")
