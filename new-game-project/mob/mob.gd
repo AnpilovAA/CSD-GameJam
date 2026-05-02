6c98a6b7b846c7e8b9d74f1a1ca4f208cddcb722
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2026-05-02 10:32:53 +0300

    Заново настроил персонажа

--- new-game-project/mob/mob.gd
+++ new-game-project/mob/mob.gd
@@ -1,11 +1,32 @@
 extends Area2D
 
+@export var speed = 300
+@export var heal = 500
 
-# Called when the node enters the scene tree for the first time.
+signal hit
+
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO
+
+func _on_body_entered(body: Node2D) -> void:
+	hit.emit()
+	$CollisionShape2D.disabled = true
+	$Timer.start(0.5)
+# Тут тоже в теории должно быть временное бессмертие
+	while not $Timer.is_stopped():
+		$CollisionShape2D.disabled = false
+
+func start(pos):
+	position = pos
+	$CollisionShape2D.disabled = false
+
+
+func _on_timer_timeout() -> void:
+	$Timer.stop()
+# Called when the node enters the scene tree for the first time.
---
+++ new-game-project/player/character_body_2d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var screen_size
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func _process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
