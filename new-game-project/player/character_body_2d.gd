46b0cb6be9a72bc3fcba0dae2b24f87616e7571c
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2026-05-05 10:20:41 +0300

    Наконец реализовал что-то типо прыжка

--- new-game-project/player/character_body_2d.gd
+++ new-game-project/player/character_body_2d.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1800.0
 
 var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-func _process(delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
+	# Двигаемся влево и вправо
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -18,25 +20,20 @@ func _process(delta: float) -> void:
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
-	
-	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
-func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
